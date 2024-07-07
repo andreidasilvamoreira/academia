2fb464ed89518cc8b9166d0db7e1a8ce5845359d
Maycon <maycondasilvamoreira@hotmail.com>
2024-07-06 21:55:23 -0300

    fix: Altera database e adiciona xdebug log no git ignore

--- app/db.php
+++ app/db.php
@@ -3,11 +3,12 @@
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule();
+
 $capsule->addConnection([
     'driver'    => env('DB_DRIVER', 'mysql'),
     'port'      => env('DB_PORT', 3306),
     'host'      => env('DB_HOST', 'localhost'),
-    'database'  => env('DB_DATABASE', 'academia'),
+    'database'  => env('DB_DATABASE', 'academia_dedei'),
     'username'  => env('DB_USERNAME', 'root'),
     'password'  => env('DB_PASSWORD', 'admin'),
     'charset'   => 'utf8',
