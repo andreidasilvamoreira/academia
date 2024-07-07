0844870a7a0db66b43a5987232acd8e1f735c00c
Maycon <maycondasilvamoreira@hotmail.com>
2024-07-06 21:59:07 -0300

    feat: Implementa as actions que estavam faltando do crud de endereços

---
+++ src/Action/Enderecos/UpdateEnderecoAction.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Action\Enderecos;
+
+use App\Action\Action;
+use App\Entities\Endereco;
+use App\Services\EnderecoService;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Log\LoggerInterface;
+
+class UpdateEnderecoAction extends Action
+{
+    public function __construct(
+        protected LoggerInterface $logger,
+        private EnderecoService $enderecoService
+
+    ) {
+        parent::__construct($logger);
+    }
+
+    protected function action(): Response
+    {
+        $idEndereco = $this->request->getAttribute('id');
+        $data = $this->request->getParsedBody();
+        $data = $this->inferDataTypes($data);
+        $enderecoEntity = Endereco::factory($data)->setId($idEndereco);
+        $enderecoCriado = $this->enderecoService->update($enderecoEntity);
+
+        return $this->respondWithData($enderecoCriado);
+    }
+}
